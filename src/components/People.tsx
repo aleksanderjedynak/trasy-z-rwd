52e9a86607042be03faef544d510e06842b3bc69
Aleksander Jedynak <aleksander.jedynak@gmail.com>
2025-01-11 01:34:38 +0100

    People - dodaje

---
+++ src/components/People.tsx
@@ -0,0 +1,17 @@
+import Footer from './Footer.tsx';
+
+const People = () => (
+  <>
+    <h1
+      style={{
+        textAlign: 'center',
+        paddingTop: '3rem',
+      }}
+    >
+      Wybierz osoba jaka chcesz sprawdzic :)
+    </h1>
+    <Footer />
+  </>
+);
+
+export { People };
